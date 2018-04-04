2a6a7fa19412ffaef725fe1bd8c027bd51b845cd
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-04 13:56:22 +0800

    pass

--- main.go
+++ main.go
@@ -127,14 +127,20 @@ func start(w http.ResponseWriter, req *http.Request) {
 	err := control.Start(vname)
 	if err != nil {
 		fmt.Println(err.Error())
+		msg, err := json.Marshal(er{Ret: "e", Msg: "开机失败", Data: err.Error()})
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		w.Write(msg)
 		return
 	}
-	msg, err := json.Marshal(er{Ret: "v", Msg: "正在开机", Data: err.Error()})
+	msg, err := json.Marshal(er{Ret: "v", Msg: "正在开机"})
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	w.Write(msg)
+
 }
 
 func passwdAPI(w http.ResponseWriter, req *http.Request) {
