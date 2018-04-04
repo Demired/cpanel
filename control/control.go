81a2c424bd7e73bcc19c1b76963edcdb34efd31f
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-04 13:36:33 +0800

    pass

--- control/control.go
+++ control/control.go
@@ -2,6 +2,8 @@ package control
 
 import (
 	"fmt"
+
+	libvirt "github.com/libvirt/libvirt-go"
 )
 
 func Start(vname string) error {
@@ -12,4 +14,13 @@ func Start(vname string) error {
 	}
 	fmt.Println("123")
 	return nil
+}
+
+func Connect() *libvirt.Connect {
+	conn, err := libvirt.NewConnect("qemu:///session")
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	return conn
 }
