fca449ab2185d6456126b4920031d6a6b275e67f
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-08 18:21:34 +0800

    pass

--- control/control.go
+++ control/control.go
@@ -3,7 +3,6 @@ package control
 import (
 	"cpanel/control"
 
-	crypt "github.com/go-crypt"
 	libvirt "github.com/libvirt/libvirt-go"
 )
 
@@ -40,17 +39,13 @@ func Reboot(vname string) error {
 }
 
 func SetPasswd(vname string, userName string, passwd string) error {
-	encryptPasswd, err := crypt.Crypt(passwd, "$6$Pk3YRrQamkzbN6wY")
-	if err != nil {
-		return err
-	}
 	dom, err := control.Connect().LookupDomainByName(vname)
 	if err != nil {
 		return err
 	}
 	s, _, err := dom.GetState()
 	if int(s) == 1 {
-		err = dom.SetUserPassword(userName, encryptPasswd, libvirt.DOMAIN_PASSWORD_ENCRYPTED)
+		err = dom.SetUserPassword(userName, passwd, libvirt.DOMAIN_PASSWORD_ENCRYPTED)
 		if err != nil {
 			return err
 		}
