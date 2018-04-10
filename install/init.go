0016d9d79899f3d7610d6aa1a8d495e6fa7596fc
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-10 13:10:18 +0800

    pass

--- install/init.go
+++ install/init.go
@@ -17,18 +17,36 @@ func Init() {
 	defer db.Close()
 
 	sql := `CREATE TABLE IF NOT EXISTS vm (
-        ID INTEGER PRIMARY KEY AUTOINCREMENT,
-        Vname CHAR(20),
-        IPv4 CHAR(30),
-        IPv6 CHAR(50),
-        LocalIP CHAR(30),
-        Vcpu INT NOT NULL,
-        Vmemory INT NOT NULL,
-        Status INT NOT NULL,
-        Mac CHAR(20),
-        Ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        Utime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-        );
-    `
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                UID INT NOT NULL,
+                Vname CHAR(20),
+                IPv4 CHAR(30),
+                IPv6 CHAR(50),
+                LocalIP CHAR(30),
+                Vcpu INT NOT NULL,
+                Vmemory INT NOT NULL,
+                Status INT NOT NULL,
+                Mac CHAR(20),
+                Ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                Utime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            );
+            // CREATE TABLE IF NOT EXISTS user (
+            //     ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            //     uname CHAR(20),
+            //     Email CHAR(20),
+            //     Passwd CHAR(32),
+            //     Tel CHAR(20),
+            //     Ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            //     Utime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            // );
+            // CREATE TABLE IF NOT EXISTS billing (
+            //     ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            //     UID INT NOT NULL,
+            //     VID INT NOT NULL,
+            //     Status INT NOT NULL,
+            //     Ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            // );
+            `
 	db.Exec(sql)
+	//开机关机的时候做计费标记
 }
