abfef0dd134e78a349bca140eaa9e52c13d53a2f
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-18 14:15:07 +0800

    带宽

--- main.go
+++ main.go
@@ -323,7 +323,7 @@ func createAPI(w http.ResponseWriter, req *http.Request) {
 		w.Write(msg)
 		return
 	}
-	_, err = stmt.Exec(tvm.Vname, tvm.Vcpu, tvm.Vmemory, tvm.Mac, tvm.Bandwidth, 1, "", "", "")
+	_, err = stmt.Exec(tvm.Vname, tvm.Vcpu, tvm.Vmemory, tvm.Mac, tvm.Bandwidth, 1)
 	if err != nil {
 		msg, _ := json.Marshal(er{Ret: "e", Msg: "写入失败", Data: err.Error()})
 		w.Write(msg)
