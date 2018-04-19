76f08fa50c8e4f7f1b7070ef337c8c9218f5142c
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-19 14:00:08 +0800

    pass

--- main.go
+++ main.go
@@ -145,12 +145,9 @@ func info(w http.ResponseWriter, req *http.Request) {
 	}
 	var vmInfo = make(map[string]string)
 	vmInfo["vname"] = ww.Vname
-	vmInfo["Vcpu"] = ww.Vcpu
-	vmInfo["Bandwidth"] = ww.Bandwidth
-	vmInfo["Vmemory"] = ww.Vmemory
-	// vmInfo["Bandwidth"] = fmt.Sprintf("%d", ww.Bandwidth)
-	// vmInfo["Vmemory"] = fmt.Sprintf("%d", ww.Vmemory)
-	// vmInfo["Vcpu"] = fmt.Sprintf("%d", ww.Vcpu)
+	vmInfo["Bandwidth"] = fmt.Sprintf("%d", ww.Bandwidth)
+	vmInfo["Vmemory"] = fmt.Sprintf("%d", ww.Vmemory)
+	vmInfo["Vcpu"] = fmt.Sprintf("%d", ww.Vcpu)
 	// vmInfo[""]
 	fmt.Println(vmInfo)
 	t, _ := template.ParseFiles("html/info.html")
