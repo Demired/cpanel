d2dbff86a7f8a99030f22a115f240c84fc0b3c77
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-04-23 15:48:46 +0800

    pass

--- main.go
+++ main.go
@@ -186,7 +186,7 @@ func loadJSON(w http.ResponseWriter, req *http.Request) {
 	var date = make(map[string]interface{})
 	var cpus [][]int
 	for _, v := range watchs {
-		date["cpus"] = append(cpus, []int{v.Ctime, v.CPU})
+		cpus = append(cpus, []int{v.Ctime, v.CPU})
 	}
 	date["cpus"] = cpus
 	dj, _ := json.Marshal(date)
