dfa6117b080d111805af3647c3ea0633ff2048f7
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-05-02 17:03:27 +0800

    var err init

--- control/control.go
+++ control/control.go
@@ -40,7 +40,7 @@ func CheckEtime(Vname string) error {
 	if err != nil {
 		return err
 	}
-	if time.Now().After(dInfo.Etime) {
+	if time.Now().Before(dInfo.Etime) {
 		return errors.New("虚拟机到期")
 	}
 	return nil
