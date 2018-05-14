fc149c81161579721a817d15c12acb166ad3926f
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-05-14 20:22:55 +0800

    forget

--- table/fields.go
+++ table/fields.go
@@ -5,30 +5,30 @@ import (
 )
 
 type Virtual struct {
-	ID         int       `json:"id"`
-	UID        int       `json:"uid"`
-	IPv4       string    `json:"ipv4"`
-	IPv6       string    `json:"ipv6"`
-	LocalIP    string    `json:"local"`
-	Vcpu       int       `json:"vcpu"`
-	Status     int       `json:"status"`
-	Vmemory    int       `json:"vmemory"` //GiB
-	Passwd     string    `json:"vpasswd"`
-	Vname      string    `json:"vname"`
-	Tag        string    `json:"tag"`
-	Br         string    `json:"br"`
-	Mac        string    `json:"mac"`
-	Sys        string    `json:"sys"`
-	Bandwidth  int       `json:"bandwidth"` //Mbps
-	Etime      time.Time `json:"etime"`     //Expire time
-	Ctime      time.Time `json:"ctime"`
-	Utime      time.Time `json:"utime"`
-	AutoPay    int       `json:"autopay"`
-	ACpu       int       `json:"acpu"`
-	ABandwidth int       `json:"abandwidth"`
-	AMemory    int       `json:"amemory"`
-	ADisk      int       `json:"adisk"`
-	AStatus    int       `json:"astatus"`
+	ID        int       `json:"id"`
+	UID       int       `json:"uid"`
+	IPv4      string    `json:"ipv4"`
+	IPv6      string    `json:"ipv6"`
+	LocalIP   string    `json:"local"`
+	Vcpu      int       `json:"vcpu"`
+	Status    int       `json:"status"`
+	Vmemory   int       `json:"vmemory"` //GiB
+	Passwd    string    `json:"vpasswd"`
+	Vname     string    `json:"vname"`
+	Tag       string    `json:"tag"`
+	Br        string    `json:"br"`
+	Mac       string    `json:"mac"`
+	Sys       string    `json:"sys"`
+	Bandwidth int       `json:"bandwidth"` //Mbps
+	Etime     time.Time `json:"etime"`     //Expire time
+	Ctime     time.Time `json:"ctime"`
+	Utime     time.Time `json:"utime"`
+	// AutoPay    int       `json:"autopay"`
+	ACpu       int `json:"acpu"`
+	ABandwidth int `json:"abandwidth"`
+	AMemory    int `json:"amemory"`
+	ADisk      int `json:"adisk"`
+	AStatus    int `json:"astatus"`
 }
 
 type Watch struct {
