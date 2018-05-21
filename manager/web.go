a30eb7456b211d7dc5ffa1e83789b5f5f0e4f1e2
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-05-21 15:05:07 +0800

    sha1

--- manager/web.go
+++ manager/web.go
@@ -63,16 +63,16 @@ func loginAPI(w http.ResponseWriter, req *http.Request) {
 		w.Write(msg)
 		return
 	}
-	//产生一个散列值得方式是 sha1.New()，sha1.Write(bytes)，然后 sha1.Sum([]byte{})。这里我们从一个新的散列开始。
 	h := sha1.New()
-	//写入要处理的字节。如果是一个字符串，需要使用[]byte(s) 来强制转换成字节数组。
 	h.Write([]byte(passwd))
-	//这个用来得到最终的散列值的字符切片。Sum 的参数可以用来都现有的字符切片追加额外的字节切片：一般不需要要。
 	bs := h.Sum(nil)
-	//SHA1 值经常以 16 进制输出，例如在 git commit 中。使用%x 来将散列结果格式化为 16 进制字符串。
 	sha1passwd := fmt.Sprintf("%x\n", bs)
 	fmt.Println(sha1passwd)
+	fmt.Println("------------")
+	fmt.Println(manager.ID)
+	fmt.Println(manager.Email)
 	fmt.Println(manager.Passwd)
+	fmt.Println("------------")
 	fmt.Println(manager.Passwd == sha1passwd)
 	if manager.Passwd != sha1passwd {
 		msg, _ := json.Marshal(tools.Er{Ret: "e", Msg: "密码错误"})
