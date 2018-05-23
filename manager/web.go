6b1c1edbeb8dbb028eb6db71bf011a514d09acd7
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-05-23 14:18:50 +0800

    manager

--- manager/web.go
+++ manager/web.go
@@ -33,29 +33,6 @@ func init() {
 	orm.RegisterDataBase("default", "sqlite3", "./db/cpanel_manager.db", 30)
 }
 
-//套餐列表 套餐模板
-func compose(w http.ResponseWriter, req *http.Request) {
-	var composes []table.Compose
-	o := orm.NewOrm()
-	res, err := o.Raw("Select * form compose where status = ?", "1").QueryRows(&composes)
-	// err := o.Read(&compose)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(res)
-	fmt.Println(composes)
-	t, _ := template.ParseFiles("html/manager/compose.html")
-	t.Execute(w, nil)
-}
-
-//套餐列表
-func composes(w http.ResponseWriter, req *http.Request) {
-	o := orm.NewOrm()
-	o.Raw("select * from Composes where status = 1")
-	fmt.Println("123")
-}
-
 // index web template
 func index(w http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("html/manager/index.html")
